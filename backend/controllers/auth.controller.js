98bbb16976c27e4b0fbf3f3e2aeb7a5ea0e97270
Evgenii Lepekho <lepehun@mail.ru>
2024-04-24 13:20:04 +0200

    feat: config basic backend logic, add user model,  add signup router and controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,60 @@
+import bcrypt from 'bcryptjs'
+import User from '../models/user.model.js'
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: 'Passwords do not match' })
+    }
+
+    const user = await User.findOne({ username })
+
+    if (user) {
+      return res.status(400).json({ message: 'Username already exists' })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
+    })
+
+    if (newUser) {
+      await newUser.save()
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        gender: newUser.gender,
+        profilePic: newUser.profilePic
+      })
+    } else {
+      res.status(400).json({ message: 'Invalid user data received' })
+    }
+
+  } catch (error) {
+    console.log('Error in signup controller', error)
+    res.status(500).json({ message: error.message })
+  }
+}
+
+export const login = (req, res) => {
+  res.send('Login User')
+  console.log('Login User')
+}
+
+export const logout = (req, res) => {
+  res.send('Logout User')
+  console.log('Logout User')
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+
+import authRoutes from './routes/auth.routes.js'
+import connectToMongoDB from './db/connectToMongoDB.js'
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+dotenv.config()
+
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+  connectToMongoDB()
+})
