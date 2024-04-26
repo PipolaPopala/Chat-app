af0fa01e65c53f3b5610b85dea3b658a1840dbcb
Evgenii Lepekho <lepehun@mail.ru>
2024-04-26 23:18:34 +0200

    feat: add message and conversation model

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -45,8 +45,8 @@ export const signup = async (req, res) => {
       res.status(400).json({ message: 'Invalid user data received' })
     }
   } catch (error) {
-    console.log('Error in signup controller', error)
-    res.status(500).json({ message: error.message })
+    console.log('Error in signup controller', error.message)
+    res.status(500).json({ error: "internal server error" })
   }
 }
 
@@ -70,8 +70,8 @@ export const login = async (req, res) => {
       profilePic: user.profilePic
     })
   } catch (error) {
-    console.log('Error in login controller', error)
-    res.status(500).json({ message: error.message })
+    console.log('Error in login controller', error.message)
+    res.status(500).json({ error: "internal server error" })
   }
 }
 
@@ -80,7 +80,7 @@ export const logout = (req, res) => {
     res.cookie('jwt', '', { maxAge: 0 })
     res.status(200).json({ message: 'Logged out successfully' })
   } catch (error) {
-    console.log('Error in logout controller', error)
-    res.status(500).json({ message: error.message })
+    console.log('Error in logout controller', error.message)
+    res.status(500).json({ error: "internal server error" })
   }
 }
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -6,7 +6,8 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true, minlength: 6 },
   gender: { type: String, required: true, enum: ['male', 'female'] },
   profilePic: { type: String, default: '' },
-})
+}, { timestamps: true } // createdAt, updatedAt
+)
 
 const User = mongoose.model('User', userSchema)
 
