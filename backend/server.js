b39d84714421fef4f24d412e1b18028bd73468b0
Evgenii Lepekho <lepehun@mail.ru>
2024-04-28 22:54:51 +0200

    feat: add protect middleware, user/message routes/controllers

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/messages.routes.js'
+import userRoutes from './routes/user.routes.js'
 import connectToMongoDB from './db/connectToMongoDB.js'
 
 const app = express()
@@ -11,9 +13,11 @@ const PORT = process.env.PORT || 5000
 dotenv.config()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
+app.use('/api/users', userRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
