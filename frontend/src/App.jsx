e444566f3e8408541b0bd6880fd668cae97adace
Evgenii Lepekho <lepehun@mail.ru>
2024-05-14 13:49:08 +0200

    fix: fix SignUp component path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-import SignUp from "./pages/signUp/SignUp";
+import SignUp from "./pages/signup/SignUp";
 import { useAuthContext } from "./context/AuthContext";
 
 function App() {
